5964aab3c4731b182801a52998e14886025162d8
Suleiman G. K. <suleimankareithi@yahoo.com>
2024-11-08 14:02:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ window.addEventListener("scroll", () => {
     }), navLi.forEach(t => {
         t.classList.remove("activeThistab"), t.classList.contains(e) && t.classList.add("activeThistab")
     })
-}), console.log("%c Designed and Developed by Vinod Jangid ", "background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;");
+}), console.log("%c Designed and Developed by Suleiman G. K. ", "background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;");
 let mybutton = document.getElementById("backtotopbutton");
 
 function scrollFunction() {
